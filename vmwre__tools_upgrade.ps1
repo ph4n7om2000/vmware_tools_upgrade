032c478c7afe7edcdb8c995bbd738081a42f138d
ph4n7om2000 <aroshlakshan@outlook.com>
2022-12-06 07:52:50 +0000

    completed_one

--- vmwre__tools_upgrade.ps1
+++ vmwre__tools_upgrade.ps1
@@ -1,18 +1,34 @@
 $URL = "https://packages.vmware.com/tools/esx/latest/windows/x64/"
+$LogFilePath= "C:\temp\VMwareToolsUpdateScript.log"
+(Get-Date).ToString() +" :  "+"Script Initated" >> $logfilepath
+$PSversion= Get-Host | Select-Object Version
+(Get-Date).ToString() +" : PowerShellversion=" + $PSversion >> $logfilepath
 $QueryVMWareToolsVersion = Invoke-WebRequest $URL -UseBasicParsing
-$VMWareToolsSetupName = $QueryVMWareToolsVersion.Links.HREF | Select -Skip 1
-$VMWareToolsNewestVersion = $VMWareToolsSetupName -replace ".*VMware-tools-" -replace "-.*"
-$VMWareToolsInstalledVersion = Get-WmiObject Win32_Product -Filter "Name like 'VMware Tools'" | Select-Object -ExpandProperty Version
-If ($VMWareToolsInstalledVersion -lt $VMWareToolsNewestVersion) 
-    {
 
+$VMWareToolsSetupName = $QueryVMWareToolsVersion.Links.HREF | Select -Skip 1
+[string]$VMWareToolsNewestVersion = $VMWareToolsSetupName -replace ".*VMware-tools-" -replace "-.*"
+$VMWareToolsInstalledVersion = Get-WmiObject Win32_Product -Filter "Name like 'VMware Tools'" | Select-Object -ExpandProperty Version
+[string]$VMWareToolsInstalledVersion = $VMWareToolsInstalledVersion.Substring(0,$VMWareToolsInstalledVersion.lastIndexOf('.'))
+If ([version]$VMWareToolsInstalledVersion -lt [version]$VMWareToolsNewestVersion) 
+    {
     $DownloadURL = $URL+$VMWareToolsSetupName
+try{
     Invoke-WebRequest -Uri $DownloadURL -OutFile "C:\temp\$VMWareToolsSetupName"
-    Write-Host "Download Finished!"
+    (Get-Date).ToString() +" :  "+"Download Finished!" >> $logfilepath
+    }
+catch{
+         (Get-Date).ToString() +" :  "+"Download Failed" >> $logfilepath
+      }
     $ArgumentList = "/S /v " + '"/qn REBOOT=R ADDLOCAL=ALL"' + "/l C:\temp\VMwareToolsSetup.log"
     $FilePath = "C:\temp\" + $VMWareToolsSetupName
-    Start-Process -FilePath $FilePath -ArgumentList $ArgumentList
+    try{
+         Start-Process -FilePath $FilePath -ArgumentList $ArgumentList
+         (Get-Date).ToString() +" :  "+"Installtion Finished!" >> $logfilepath
+        }
+    catch{
+            (Get-Date).ToString() +" :  "+"Installtion failed" >> $logfilepath
+          }
+         }    
+Else {(Get-Date).ToString() +" :  "+"VMware latest verison is already installed!" >> $logfilepath}
 
-     }
-
-     Else {Write-Host "VMware latest verison is already installed!"}
+(Get-Date).ToString() +" :  "+"Script has been executed successfully" >> $logfilepath
